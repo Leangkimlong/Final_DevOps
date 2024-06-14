4ecb9751404f8a01c4ab290658cf1b330493a29a
Leangkimlong <mizterlong95@gmail.com>
2024-06-14 14:33:15 +0700

    commit

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -3,8 +3,8 @@ use App\Models\Student;
 use function PHPUnit\Framework\assertTrue;
 test('create student', function () {
     $student = Student::create([
-        'name' => 'John Doe',
-        'email' => '<your_name>@itc.edu.kh',
+        'name' => 'Leang Kimlong',
+        'email' => 'leangkimlong>@itc.edu.kh',
         'phone' => '012 345 678',
         'dob' => '2003-10-01'
     ]);
